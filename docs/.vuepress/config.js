03b409b14f2bf67b1e4d6d42591086a1b2a66220
Underglaze-Blue <lepapillonangel@gmail.com>
2021-03-21 00:21:47 +0800

    基本配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,12 +12,13 @@ const internal_path = fs.readdirSync(js_filePath).filter(item => item !== 'READM
 
 
 module.exports = {
-  base: '/lodash-analysis/',
+  base: '/blog/',
   theme: 'reco',
   dest: 'dist',
-  title: 'Lodash 源码分析',
-  description: 'Analysis lodash.js deeply',
+  title: 'Blog',
+  description: 'Lighting the way',
   head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', {
       rel: 'stylesheet',
@@ -37,33 +38,34 @@ module.exports = {
     }
   },
   themeConfig: {
-    smoothScroll: true,
-    repo: 'Underglaze-Blue/lodash-analysis',
-    editLinks: true,
-    docsDir: 'docs',
-    editLinkText: ' 在 GitHub 上编辑此页',
-    lastUpdated: ' 上次更新',
-    docsBranch: 'main',
-    searchMaxSuggestions: 10,
-    nav: [
+    author: 'Himawari',
+    authorAvatar: '/avatar.png',
+    type: "blog",
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: '分类' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: '标签'      // 默认文案 “标签”
+      },
+      socialLinks: [     // 信息栏展示社交信息
+        { icon: 'reco-github', link: 'https://github.com/Underglaze-Blue' },
+      ]
+    },
+    friendLink: [
       {
-        text: 'Lodash',
-        items: [
-          { text: 'Lodash文档', link: 'https://www.lodashjs.com/' },
-          { text: 'Lodash GitHub', link: 'https://github.com/lodash/lodash' }
-        ]
+        title: 'vuepress-theme-reco',
+        desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+        logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: 'https://vuepress-theme-reco.recoluan.com'
       },
       {
-        text: 'Mozilla',
-        link: 'https://developer.mozilla.org/zh-CN/'
-      }
-    ],
-    sidebar: [
-      {
-        title: 'Internal',   // 必要的
-        collapsable: true,
-        sidebarDepth: 0,
-        children: handleChildren('internal', internal_path)
+        title: 'Lodash 源码分析',
+        desc: 'Lodash 源码分析 - 个人写作',
+        logo: "lodash.png",
+        link: 'https://underglaze-blue.github.io/lodash-analysis/'
       }
     ]
   },
