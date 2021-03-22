b3fb8c5ca902a8d6047edf6fa9324de6f57b3f34
Underglaze-Blue <lepapillonangel@gmail.com>
2021-03-22 16:29:11 +0800

    fix ci.yml

--- actions.sh
+++ actions.sh
@@ -2,7 +2,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 npm run build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
+cd dist # 进入生成的文件夹
 
 # deploy to github
 githubUrl=https://Underglaze-Blue:${GITHUB_TOKEN}@github.com/Underglaze-Blue/blog.git
@@ -14,4 +14,4 @@ git commit -m "deploy"
 git push -f $githubUrl master:gh-pages # 推送到github
 
 cd -
-rm -rf docs/.vuepress/dist
+rm -rf dist
